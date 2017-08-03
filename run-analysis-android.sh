9de240beb27a5f80f5f38ddd0331895259a1ad4e
Maria Kechagia <mkechagia@aueb.gr>
2017-08-03 11:51:41 +0300

    Add comments in the android script

--- run-analysis-android.sh
+++ run-analysis-android.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-### Don't forget to study the README.md file first and provide the right inputs!
-### Comment out the commands you don't want to execute!
-### May you will need only the set operations in the end!
+### Don't forget to study the instructions.txt file first and provide the right inputs!
+### Comment out the commands you don't want to execute! Otherwise, the analysis will take too long.
+### May you will only need to run the set operations in the end of the script!
+### Already processed data can be found in the eRec-data repo (https://github.com/mkechagia/eRec-data.git).
 
 # get the directory where the eRec repo is downloaded
 cwd=`pwd`
@@ -27,13 +28,17 @@ python $cwd/api/processing/doc/api_doc_exceptions.py $ANDROID/sources/ android
 
 cd $cwd/api/JSON/
 
-# replace "sample-app" with the app to be analyzed and "XX" with the API version of the app --- one app for each API version
+# replace "sample-app" with the app to be analyzed and "XX" with the API version of the app;
+# one app for each API version is sufficient since Soot (using Dexpler) analyzes also the Android libraries used by an .apk.
+# Note: for this step, you need to provide the android-platforms and have run .apk analysis (see step 2. Apps) on specific apps
 python $cwd/api/processing/source/api_source_code_exceptions.py $cwd/apps/apk/sample-app XX android
 
 ### c) total API versions in .json file (one file for doc and one for source code) ###
 
 cd $cwd/api/JSON/
 
+# this command will take some time depending on the number of API versions you want to analyze
+# prepared .json files can be found in the eRec-data repo in the JSON directory
 python $cwd/api/processing/total_api_versions.py $cwd/api/JSON/ android
 
 
@@ -49,7 +54,7 @@ cd ..
 python $cwd/apps/processing/app_exceptions.py $cwd/apps/apk/ $cwd/api/JSON/
 
 # After some filtering of the $cwd/apps/android-undoc-excps.txt file,
-# we can get the produced file $cwd/apps/android-undoc-excps-processed.txt
+# we can get the produced file $cwd/apps/android-undoc-excps-processed.txt; we provide such a sample file in the apps folder of the eRec-data.
 
 
 ### 3. Crashes ###
